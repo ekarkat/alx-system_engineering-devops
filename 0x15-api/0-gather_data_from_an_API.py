1eff2433d6be2354b4fdfcfac487c5ffca20aed5
ekarkat <ekarkat@gmail.com>
2024-01-02 23:02:53 +0000

    task0

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+""" TAsk0 model """
+
+from sys import argv
+import requests
+
+
+if __name__ == "__main__":
+    # task 0 function
+    id = "2"
+    params_todo = {"userId": id}
+    params_user = {"id": id}
+    url_todo = "https://jsonplaceholder.typicode.com/todos"
+    url_user = "https://jsonplaceholder.typicode.com/users"
+
+    response_todo = requests.get(url_todo, params=params_todo)
+    data_todo = response_todo.json()
+
+    response_user = requests.get(url_user, params=params_user)
+    data_user = response_user.json()
+
+    def todo_true(list):
+        n = 0
+        for dic in list:
+            if dic.get("completed"):
+                n += 1
+        return (n)
+
+    tr = todo_true(data_todo)
+    name = data_user[0].get("name")
+
+    output = f"Employee {name} is done with tasks({tr}/{len(data_todo)}):"
+    print(output)
+
+    for dic in data_todo:
+        if dic.get("completed"):
+            print(dic.get("title"))
