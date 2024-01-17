c68cff2725fa88a4e561a2c253f8a5590f61f1a5
ekarkat <ekarkat@gmail.com>
2024-01-17 18:33:05 +0000

    tas0,1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -5,16 +5,15 @@ import requests
 
 def top_ten(subreddit):
     """Return returns the hot list of a sub"""
-    # url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    # headers = {"User-Agent": "ALX 0x16.api_advanced"}
-    # response = requests.get(url, headers=headers, allow_redirects=False)
-    # if response.status_code == 200:
-    #     results = response.json().get("data").get("children")
-    #     i = 0
-    #     for title in results:
-    #         if i < 10:
-    #             print(title.get("data").get("title"))
-    #         i = i + 1
-    # else:
-    #     print(None)
-    print(None)
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {"User-Agent": "ALX 0x16.api_advanced"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        results = response.json().get("data").get("children")
+        i = 0
+        for title in results:
+            if i < 10:
+                print(title.get("data").get("title"))
+            i = i + 1
+    else:
+        print(None)
