f2bb0993b759a9aa8e53e9ccd670fbaa0a3a331f
ekarkat <ekarkat@gmail.com>
2024-01-17 18:31:12 +0000

    tas0,1

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,12 +7,13 @@ import requests
 
 def number_of_subscribers(subreddit):
     """Return returns the number of subscribers"""
-    headers = {"User-Agent": "ALX 0x16.api_advanced"}
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    response = requests.get(url, allow_redirects=False, headers=headers)
-    if response.status_code == 200:
-        results = response.json().get("data")
-        subs = results.get("subscribers")
-        return output
-    else:
-        return 0
+    # headers = {"User-Agent": "ALX 0x16.api_advanced"}
+    # url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    # response = requests.get(url, allow_redirects=False, headers=headers)
+    # if response.status_code == 200:
+    #     results = response.json().get("data")
+    #     subs = results.get("subscribers")
+    #     return output
+    # else:
+    #     return 0
+    return 0
