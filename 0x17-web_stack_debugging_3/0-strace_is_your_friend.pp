600e0a955bd7b6847448f59454767c5fdac5dcb5
ekarkat <ekarkat@gmail.com>
2024-01-17 22:38:59 +0000

    tas0

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # Fixes php issue
 
 exec { 'fix-wordpress':
-  command => '/usr/local/bin/:/bin/sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  command => '/bin/sed -i 's/phpp/php/g' /var/www/html/wp-settings.php',
 }
